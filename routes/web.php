13ebaf3d3b28222261c2798312ef3e02eaaed9fc
Miqueias Penha <miqueiaspenha@gmail.com>
2023-01-29 19:03:49 -0300

    add jetstream

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,18 @@ use App\Http\Livewire\{
 |
 */
 
-Route::get('/tweets', ShowTweets::class);
+Route::get('/tweets', ShowTweets::class)->middleware('auth');
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
